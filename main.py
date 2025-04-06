4c8022d114b6eb99375adc93e34f82e3aee5e966
nghiango1 <ducnghia.tin47@gmail.com>
2025-04-06 12:07:48 +0700

    CH3 Lession 2 - Arguments

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import sys
+
 from stats import (
-    get_chars_dict,
-    get_book_text,
     chars_dict_to_sorted_list,
+    get_book_text,
+    get_chars_dict,
     get_number_of_words,
 )
 
@@ -20,7 +22,10 @@ def print_report(book_path, num_words, sorted_list):
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_number_of_words(text)
     chars_dict = get_chars_dict(text)
