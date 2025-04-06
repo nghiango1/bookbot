9ae9efdd90d940a35e0239060f7c1a8f60d3336e
nghiango1 <ducnghia.tin47@gmail.com>
2025-04-06 11:33:45 +0700

    Lession 4

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ def get_book_text(path_to_file):
         return file_contents
 
 
+def get_book_number_of_words(path_to_file):
+    file_contents = get_book_text(path_to_file)
+    num_words = len(file_contents.split())
+    return f"{num_words} words found in the document"
+
+
 def main():
-    get_book_text("frankenstein.txt")
+    print(get_book_number_of_words("books/frankenstein.txt"))
 
 
 main()
