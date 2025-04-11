6acf16c681580f22ad72a5448cc918f6db93775c
nghiango1 <ducnghia.tin47@gmail.com>
2025-04-11 22:19:03 +0700

    add more test for leaf node

--- courses/static_site_generator/src/test_leafnode.py
+++ courses/static_site_generator/src/test_leafnode.py
@@ -16,6 +16,31 @@ class TestTextNode(unittest.TestCase):
             node.to_html(), '<a href="https://www.google.com">Click me!</a>'
         )
 
+    def test_leaf_init(self):
+        exception = None
+        try:
+            node = LeafNode(None, None)
+        except Exception as e:
+            exception = e
+        assert exception is not None, "Still able to create LeafNode without Value"
+
+        node = LeafNode(None, "Value_Mock")
+        assert isinstance(node, LeafNode), "Can't set tag to be None"
+
+    def test_to_html(self):
+        node = LeafNode(None, "Value_Mock")
+        node.value = None  # Force node.value to be None
+
+        exception = None
+        try:
+            node.to_html()
+        except ValueError as e:
+            exception = e
+        except Exception as e:
+            print("Got expected error")
+            raise e
+        assert exception is not None, "node.to_html() mustnot work with node.value None"
+
 
 if __name__ == "__main__":
     unittest.main()
